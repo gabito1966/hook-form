6d4fdcf219372734563774643b0584da199ce9ed
Gabriel Garcia <gabito1966@gmail.com>
2024-04-25 13:52:51 -0300

    Practica de hooks de formulario

---
+++ src/HooksApp.jsx
@@ -0,0 +1,12 @@
+import { FormularioComponent } from "./components/FormularioComponent"
+
+export const HooksApp = () => {
+  return (
+    <>
+      <h1>Aplicacion de hooks</h1>
+      <hr />
+      <FormularioComponent></FormularioComponent>
+    </>
+  )
+}
+
